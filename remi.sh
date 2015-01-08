39e39595d8912f526eebae9b15d4a97f1269aa02
Wiktor <wikii122@gmail.com>
2015-01-08 17:59:56 +0100

    Added ability to revert

--- remi.sh
+++ remi.sh
@@ -9,7 +9,7 @@ Help:\n
 Options\n
 \t--list - list all deleted files from this directory.\n
 \t--list-all - list all deleted files.\n
-\t--revert filename [version] - bring back file of given name to current directory. If more than one exists, version is required.\n
+\t--revert index - bring back file with given index to current directory. Invalidates other indexes.\n
 "
 
 if [[ $# -lt 1 ]]
@@ -18,44 +18,82 @@ then
 	exit 1
 fi
 
+metadir="$HOME/.bin_tmp/"
+metafile="$metadir""metadata"
+
+function garbage_collector {
+	if [[ -w $metafile ]];
+	then
+		nfile=$(awk 'NF {
+		if ($4 > (systime() - 48*60*60)) {
+				print
+			} else {
+				system("rm '"$metadir"'"$1)
+			}
+		}' $metafile)
+		printf "$nfile" > $metafile
+	fi
+}
+
 OPTION=$1
+
 case $OPTION in
-	"--list") 
+	"--list")
+		garbage_collector
 		awk -v path=$(readlink -f $PWD) 'BEGIN {
 			print "List of files in current directory currently on index"
 			print "Index\tName\tDeletion Date"
 			i=0
 		}
-		{
+		NF {
+			i++
 			if ($3 == path) {
 				print i "\t" $2 "\t" strftime("%F %T", $4)
 			}
-			i++
 		}
 		END {
 			if (i == 0) {
 				print "No files found."
 			}
 		}
-		' ~/.bin_tmp/metadata
+		' $metafile
 		;;
 	"--list-all") echo list-all
+		garbage_collector
 		awk -v path=$(readlink -f $PWD) 'BEGIN {
 			print "List of files in current directory currently on index"
 			print "Index\tName\tDeletion Date\t\tPath"
 			i=0
 		}
-		{
+		NF {
 			i++
-			print i "\t" $2 "\t" strftime("%F %T", $4) "\t" $3 
+			print i "\t" $2 "\t" strftime("%F %T", $4) "\t" $3
 		}
 		END {
 			if (i == 0) {
 				print "No files found."
 			}
-		}' ~/.bin_tmp/metadata
+		}' $metafile
 		;;
-	"--revert") echo reverting ;;
+	"--revert") 
+		if [[ $# -ne 2 ]];
+		then
+			echo -e $USAGE
+		fi
+		nol=`wc -l < $metafile`
+		index=$2
+		if [[ (! -w $metafile) || (($nol -lt $index)) ]];
+		then
+			echo "File index does not match"
+			exit 1
+		fi
+		filedata=(`sed "$index"'q;d' $metafile`)
+		tar -xvzf $metadir${filedata[0]}
+		rm $metadir${filedata[0]}
+		new_file=`awk 'NR!~'"${index}" $metafile`
+		echo "$new_file" > $metafile
+		garbage_collector
+	;;
 	"--help") echo -e $USAGE ;;
 	*)
 		files=$@
@@ -68,7 +106,7 @@ case $OPTION in
 				exit 1
 			fi
 			if [[ ! -r $file ]];
-			then 
+			then
 				echo "$file cannot be read"
 				exit 1
 			fi
@@ -77,10 +115,10 @@ case $OPTION in
 				echo "$file cannot be deleted."
 				exit 1
 			fi
-			md5=`md5sum $file` 
+			md5=`md5sum $file`
 			tar -cvzf ~/.bin_tmp/$md5
 			rm -rf $file
-			echo "$md5 `dirname $(readlink -f remi.sh)` `date -u +%s`" >> ~/.bin_tmp/metadata
+			echo "$md5 `dirname $(readlink -f remi.sh)` `date -u +%s`" >> $metafile
 		done
 esac
 
